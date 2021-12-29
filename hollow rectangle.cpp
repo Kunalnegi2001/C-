e0d16d5d51470b80f82e2ec9774e8b9a9c3d57a0
Kunal Negi <kunalnegi2001@gmail.com>
2021-12-29 11:11:58 +0530

    hollow rectangle
    hollow rectangle

---
+++ hollow rectangle.cpp
@@ -0,0 +1,28 @@
+/* Hollow Rectangle */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int c,r;
+    cout<<"Rows ";
+    cin>>r;
+    cout<<"columns ";
+    cin>>c;
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=c;j++){
+            
+            if(i==1 || i==r || j==1 || j==c){
+            cout<<"*";}
+            else{cout<<" ";}
+            
+        }
+        cout<<endl;
+    }
+    
+    
+    
+    return 0;
+}
+
+
